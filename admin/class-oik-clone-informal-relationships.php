a9e359632e2648657d463881e4abd32acc9e549f
Herb Miller <herb@bobbingwide.com>
2019-11-21 13:56:26 +0000

    Issue #38 - map the ref attr used in the reusable blocks block

--- admin/class-oik-clone-informal-relationships.php
+++ admin/class-oik-clone-informal-relationships.php
@@ -157,6 +157,7 @@ abstract class OIK_clone_informal_relationships {
 	    , 'mediaId'
 	    , 'productId'
 	    , 'products'
+	    , 'ref'
                                );
     // $this->valid_tokens = apply_filters( "oik_clone_valid_tokens", $this->valid_tokens );
     $this->valid_tokens = array_flip( $this->valid_tokens );                           
